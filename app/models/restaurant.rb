c72075589a6a7ce2e0076afb1c3ffc863c549e94
Thomas Catz <thomas@spleatapp.com>
2022-01-19 17:41:30 -0300

    ejercicio 2 :)

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :delete_all
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian],
+                                                    message: '%{value} is not a valid category' }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Pizza Zero', address: 'Libertador 3400', phone_number: '011 4223-1989', category: 'italian')
+Restaurant.create(name: 'Sushi Uno', address: 'Libertad 3400', phone_number: '011 4711-0089', category: 'japanese')
+Restaurant.create(name: 'Burger Tres', address: 'Alcorta 900', phone_number: '011 4093-2189', category: 'french')
+Restaurant.create(name: 'Ching Chong Dos', address: 'Santa Fe 2300', phone_number: '011 4743-1189', category: 'chinese')
+Restaurant.create(name: 'Hazard Cinco', address: 'Urugay 2100', phone_number: '011 4743-6229', category: 'belgian')
